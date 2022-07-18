0209ca9d662225e18cb8528af133add690ac7f12
Bonus156 <Bonus.by@mail.ru>
2022-07-18 19:37:21 +0300

    init: 1 test fall 19 pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketPairs = Object.fromEntries(bracketsConfig);
+  let openBrackets = [];
+  for (let i = 0; i < bracketsConfig.length; i++){
+    openBrackets.push(bracketsConfig[i][0]); 
+  }
+  let stack = [];   
+  for (let bracket = 0; bracket < str.length; bracket++){
+    if (openBrackets.includes(str[bracket])) {
+      if (str[bracket] === bracketPairs[str[bracket]]) {
+        if (stack[stack.length - 1] === str[bracket]){
+          stack.pop();
+          continue;
+        }
+      }
+      stack.push(str[bracket]);
+      continue;
+    }
+    if (str[bracket] === bracketPairs[stack[stack.length - 1]]) {
+      stack.pop();
+    }     
+  }  
+  return stack.length === 0;  
 }
+
+
+
